c26b0acf41b95de74008fe4e13627a7033eb310d
Oskar Altena <altena.oskar@gmail.com>
2026-08-16 21:01:34 +1000

    something or rather idk cunt like some fucking niri config shit

--- niri/.config/niri/scripts/set_background.sh
+++ niri/.config/niri/scripts/set_background.sh
@@ -1,3 +1,6 @@
 BG_ID=3737237256
-MANGOHUD=0 linux-wallpaperengine --screen-root DP-1 --screen-root DP-2 --screen-root HDMI-A-1 --bg "$BG_ID" --scaling fill --clamp border
+MANGOHUD=0 linux-wallpaperengine --screen-root DP-1, --screen-root DP-2, --screen-root HDMI-A-1 --bg "$BG_ID" --scaling fill --clamp border --profile=fast --layer background 
+# MANGOHUD=0 linux-wallpaperengine --screen-root eDP-1 --bg "$BG_ID" --scaling fill --clamp border
+
+
 
