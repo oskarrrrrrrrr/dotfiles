e510fdf23376d3e86439042c5c75f3c8bb173ee3
Oskar Altena <altena.oskar@gmail.com>
2026-08-18 08:10:42 +1000

    something or rather, idk gang i just roll with punches

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,10 @@
 require("config.lazy")
+
+vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>j", "<C-w>jh")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
+vim.keymap.set("n", "<leader>l", "<C-w>l")
+
+vim.keymap.set("n", "<leader>sh", "<C-w>s")
+vim.keymap.set("n", "<leader>sv", "<C-w>v")
+
